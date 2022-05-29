a95df1a155f0f75e4671e85bc22aee707b449d76
Soledad Marini <msmarinip@gmail.com>
2022-05-29 15:32:45 -0300

    rutas usuario post /new y put / validan campos y el insert de usuasrio encripta la password

---
+++ controllers/errMsg.js
@@ -0,0 +1,9 @@
+const firstNameReq = 'El nombre es obligatorio.';
+const lastNameReq = 'El apellido es obligatorio.';
+const idInvalid = 'El no existe un usuario con ese id.';
+
+module.exports = {
+    firstNameReq,
+    lastNameReq,
+    idInvalid
+}
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,59 @@
 const { Router } = require('express');
-const User = require('../models/Users');
+const { check, body, request } = require('express-validator');
 
+const User = require('../models/Users');
+const { createUser, updateUser } = require('../controllers/user');
+const { validateFields } = require('../middlewares/validateFields');
+const { firstNameReq, lastNameReq, idInvalid } = require('../controllers/errMsg');
 const router = Router();
 
-router.post('/new', async (req, res) => {
+router.post(
+    '/new',
+    [
+        check('email', 'El email es obligatorio.').not().isEmpty(),
+        check('email', 'El email no es válido.').isEmail(),
+        check('email').custom(value => {
+            return User.find({email:value}).then(user => {
+              if (user.length>0) {
+                return Promise.reject('Ya hay un usuario con ese email.');
+             }
+            });
+          }),
+        check('password', 'La contraseña es obligatoria.').not().isEmpty(),
+        check('password', 'La contraseña debe tener al menos 7 caracteres.')
+            .not()
+            .isIn(['123456', 'password1', 'god123'])
+            .withMessage('No es una constraseña segura')
+            .isLength({ min: 6 })
+            .matches(/\d/),
+        check('firstName', firstNameReq).not().isEmpty(),
+        check('lastName', lastNameReq).not().isEmpty(),
+        validateFields
+    ],
+     createUser
+);
+router.put(
+    '/',
+    [
+        check('id').custom(value => {
+            return User.findById(value).then(user => {
+              if (user.length<1) {
+                return Promise.reject(idInvalid);
+             }
+            });
+          }),
+        check('firstName', firstNameReq).not().isEmpty(),
+        check('lastName', lastNameReq).not().isEmpty(),
+        validateFields
+    ],
+     updateUser
+);
+
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
     try {
-        const newUser = new User(req.body)
-        await newUser.save()
-
-        res.status(201).json({
-            ok: true,
-            user: newUser
-        })
-    } catch (error) {
-        res.json({
-            ok: false,
-            msg: error
-        })
-    }
-});
-
-
-router.get('/:userId', async (req, res) => {
-    const { userId } = req.params;
-    try {
-        const user = await User.findById(userId).select('_id userEmail userIsActive userIsAdmin userCreationDate userIsGoogle userFirstName userLastName userIsSuperAdmin');
+        const user = await User.findById(id).select('_id email isActive isAdmin creationDate isGoogle firstName lastName isSuperAdmin');
         res.status(201).json(user);
     } catch (error) {
         res.status(404).send('No existe un usuario con el id seleccionado')
@@ -34,9 +63,9 @@ router.get('/:userId', async (req, res) => {
 
 
 router.get('/auth', async (req, res) => {
-    const { userEmail, userPassword } = req.body;
+    const { email, password } = req.body;
 
-    
+
 
 })
 
