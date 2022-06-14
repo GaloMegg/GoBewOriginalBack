bf2e408b4dbfbe34564aadf16ca843de32263f46
Soledad Marini <msmarinip@gmail.com>
2022-06-14 11:17:56 -0300

    feat: ruta /users/userIsGoogleByMail/:userEmail chequea si el usuario es de google

--- routes/company.js
+++ routes/company.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
-const { getCompany, createCompany, updateCompany } = require("../controllers/company");
+const { getCompany, createCompany, updateCompany, getActiveCompany } = require("../controllers/company");
 
 const router = Router();
 
 
 router.post("/", createCompany);
 
+router.get("/getActive", getActiveCompany)
 router.get("/:companyId", getCompany)
 
 router.put("/:companyId", updateCompany)
