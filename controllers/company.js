2ffd9c5da0f423dbaa5dae09e4656cbbd3fb3c82
Soledad Marini <msmarinip@gmail.com>
2022-06-14 13:05:59 -0300

    fix: en las rutas de company

--- controllers/company.js
+++ controllers/company.js
@@ -3,12 +3,27 @@ const Company = require("../models/Company");
 
 const createCompany = async (req, res) => {
     const { companyName, companyEmail, companyPhone, companyAddress, companyDescription, companyFacebook, companyTwitter, companyInstagram, companyMap, companyIsActive } = req.body;
+    // console.log(req.body);
     try {
         const newCompany = new Company({ companyName, companyEmail, companyPhone, companyAddress, companyDescription, companyFacebook, companyTwitter, companyInstagram, companyMap, companyIsActive });
+        // console.log(newCompany);
         await newCompany.save();
         res.status(201).json({
             ok: true,
-            newCompany
+            company:{
+                companyId: newCompany._id,
+                companyName: newCompany.companyName,
+                companyEmail: newCompany.companyEmail,
+                companyPhone: newCompany.companyPhone,
+                companyAddress: newCompany.companyAddress,
+                companyDescription: newCompany.companyDescription,
+                companyFacebook: newCompany.companyFacebook,
+                companyTwitter: newCompany.companyTwitter,
+                companyInstagram: newCompany.companyInstagram,
+                companyMap: newCompany.companyMap,
+                companyIsActive: newCompany.companyIsActive
+
+            }
         });
     } catch (error) {
         res.status(501).json({
@@ -25,6 +40,7 @@ const getActiveCompany = async (req, res) => {
         res.status(201).json({
             ok: true,
             company : {
+                companyId: company[0]._id,
                 companyName: company[0].companyName,
                 companyEmail: company[0].companyEmail,
                 companyPhone: company[0].companyPhone,
@@ -51,7 +67,19 @@ const getCompany = async (req, res) => {
         const company = await Company.findById(companyId.toString());
         res.status(201).json({
             ok: true,
-            company
+            company: {
+                companyId: company._id,
+                companyName: company.companyName,
+                companyEmail: company.companyEmail,
+                companyPhone: company.companyPhone,
+                companyAddress: company.companyAddress,
+                companyDescription: company.companyDescription,
+                companyFacebook: company.companyFacebook,
+                companyTwitter: company.companyTwitter,
+                companyInstagram: company.companyInstagram,
+                companyMap: company.companyMap,
+
+            }
         });
     } catch (error) {
         res.status(501).json({
@@ -64,9 +92,11 @@ const getCompany = async (req, res) => {
 
 const updateCompany = async (req, res) => {
     const { companyId } = req.params;
+    // console.log(companyId)
     const { companyName, companyEmail, companyPhone, companyAddress, companyDescription, companyFacebook, companyTwitter, companyInstagram, companyMap, companyIsActive } = req.body;
+    // console.log(req.body);
     try {
-        const company = await Company.findByIdAndUpdate(companyId.toString(), { companyName, companyEmail, companyPhone, companyAddress, companyDescription, companyFacebook, companyTwitter, companyInstagram, companyMap, companyIsActive });
+        const company = await Company.findByIdAndUpdate(companyId, { companyName, companyEmail, companyPhone, companyAddress, companyDescription, companyFacebook, companyTwitter, companyInstagram, companyMap, companyIsActive });
         res.status(201).json({
             ok: true,
             company
